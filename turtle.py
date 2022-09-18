78a06f1d8d9b3c5641e4603c98fa07322ecb3604
Ashrin K.C <ashrinkc3@gmail.com>
2022-09-18 17:58:44 +0545

    first practice

---
+++ turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+max_length = 250
+increment = 10
+
+
+def draw_spiral(a_turtle, line_length):
+    if line_length > max_length:
+        return
+    a_turtle.forward(line_length)
+    a_turtle.right(90)
+    draw_spiral(a_turtle, line_length + increment)
+
+
+charlie = turtle.Turtle(shape="turtle")
+charlie.pensize(5)
+charlie.color("red")
+draw_spiral(charlie, 10)
+turtle.done
